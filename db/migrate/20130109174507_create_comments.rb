ed804341823030bbbd1f638b84a82b7837cb20f6
Ken Ohata <ohata@undapt.jp>
2013-01-10 02:47:53 +0900

    reject null column

--- db/migrate/20130109174507_create_comments.rb
+++ db/migrate/20130109174507_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.integer :board_id
-      t.text :content
+      t.integer :board_id, null: false
+      t.text :content, null: false
 
       t.timestamps
     end
